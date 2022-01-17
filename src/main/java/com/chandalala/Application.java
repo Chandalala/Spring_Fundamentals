134ec6b606ba4e8f06a1eda0b302fa4c1bdb4f8c
Tinashe Chandalala <Tinashe.Chandalala@entelect.co.za>
2022-01-17 21:21:21 +0200

    Created application context xml file

--- src/main/java/com/chandalala/Application.java
+++ src/main/java/com/chandalala/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
 
+
         SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
         System.out.printf(speakerService.findAll().get(0).getName());
     }
--- src/main/java/com/chandalala/service/SpeakerServiceImpl.java
+++ src/main/java/com/chandalala/service/SpeakerServiceImpl.java
@@ -1,15 +1,21 @@
 package com.chandalala.service;
 
 import com.chandalala.model.Speaker;
+import com.chandalala.repository.SpeakerRepository;
 import com.chandalala.repository.SpeakerRepositoryImpl;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    SpeakerRepositoryImpl speakerRepository = new SpeakerRepositoryImpl();
+    SpeakerRepository speakerRepository;
+
     @Override
     public List<Speaker> findAll() {
         return speakerRepository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
     }
 }
